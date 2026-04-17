f30755810057115655720ce699ee4de3497f651f
Anna Marchenay <anna.marchenay@hubone.fr>
2026-04-17 11:13:34 +0200

    Implement Ktor convention plugin and apply to data modules
    Created `JvmKtorConventionPlugin` to centralize Ktor and Kotlin Serialization configuration. This plugin applies the Kotlin serialization plugin and adds the Ktor dependency bundle.
    
    Updated the following modules to use the new `runique.jvm.ktor` plugin:
    - **app**
    - **auth:data**
    - **core:data**
    - **run:network**
    
    Registered the new plugin in `build-logic/convention/build.gradle.kts`.

---
+++ build-logic/convention/src/main/java/JvmKtorConventionPlugin.kt
@@ -0,0 +1,23 @@
+import com.marchenaya.convention.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+class JvmKtorConventionPlugin : Plugin<Project> {
+    private companion object {
+        private const val SERIALIZATION = "org.jetbrains.kotlin.plugin.serialization"
+        private const val IMPLEMENTATION = "implementation"
+        private const val KTOR = "ktor"
+    }
+
+    override fun apply(target: Project) {
+        target.run {
+            pluginManager.apply(SERIALIZATION)
+
+            dependencies {
+                IMPLEMENTATION(libs.findBundle(KTOR).get())
+            }
+        }
+    }
+
+}
