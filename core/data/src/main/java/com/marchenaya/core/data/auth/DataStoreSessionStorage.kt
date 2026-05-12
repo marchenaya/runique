7e1d52cecb35a6ae02590449f25a06109b43ae2a
anna.marchenay <anna.marchanay@hubone.fr>
2026-05-12 17:11:37 +0200

    Refactor DataStoreSessionStorage to simplify implementation
    - Simplify observeAuthInfo by removing a redundant map operation on the DataStore flow.
    - Clean up the set method by removing an unnecessary null check, directly passing the new value to updateData.

--- core/data/src/main/java/com/marchenaya/core/data/auth/DataStoreSessionStorage.kt
+++ core/data/src/main/java/com/marchenaya/core/data/auth/DataStoreSessionStorage.kt
@@ -4,23 +4,14 @@ import androidx.datastore.core.DataStore
 import com.marchenaya.core.domain.AuthInfo
 import com.marchenaya.core.domain.SessionStorage
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 
 class DataStoreSessionStorage(
     private val dataStore: DataStore<AuthInfo?>
 ) : SessionStorage {
 
-    override fun observeAuthInfo(): Flow<AuthInfo?> {
-        return dataStore.data.map { authInfo ->
-            authInfo
-        }
-    }
+    override fun observeAuthInfo(): Flow<AuthInfo?> = dataStore.data
 
     override suspend fun set(newInfo: AuthInfo?) {
-        if (newInfo == null) {
-            dataStore.updateData { null }
-            return
-        }
         dataStore.updateData { _ ->
             newInfo
         }
