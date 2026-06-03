6251bde9fe26ca870d598b5e029a7d2ec40c4eb7
anna.marchenay <anna.marchanay@hubone.fr>
2026-06-03 15:52:53 +0200

    Implement login functionality and input validation in the auth module
    * Implemented the login method in AuthRepository and AuthRepositoryImpl, including session storage for authentication tokens.
    * Created LoginRequest and LoginResponse DTOs for authentication network calls.
    * Added form validation logic to LoginViewModel using UserDataValidator to manage the login button state.
    * Implemented login execution and event handling in LoginViewModel to manage UI states and navigation.
    * Updated LoginScreen to support email keyboard types and improved button enabled/loading states.
    * Renamed LoginEvent.Success to LoginSuccess for improved naming consistency.

--- auth/data/src/main/java/com/marchenaya/auth/data/AuthRepositoryImpl.kt
+++ auth/data/src/main/java/com/marchenaya/auth/data/AuthRepositoryImpl.kt
@@ -2,13 +2,41 @@ package com.marchenaya.auth.data
 
 import com.marchenaya.auth.domain.AuthRepository
 import com.marchenaya.core.data.networking.post
+import com.marchenaya.core.domain.AuthInfo
+import com.marchenaya.core.domain.SessionStorage
 import com.marchenaya.core.domain.util.DataError
 import com.marchenaya.core.domain.util.EmptyResult
+import com.marchenaya.core.domain.util.Result
+import com.marchenaya.core.domain.util.asEmptyDataResult
 import io.ktor.client.HttpClient
 
 class AuthRepositoryImpl(
-    private val httpClient: HttpClient
+    private val httpClient: HttpClient,
+    private val sessionStorage: SessionStorage
 ) : AuthRepository {
+
+    override suspend fun login(
+        email: String,
+        password: String
+    ): EmptyResult<DataError.Network> {
+        val result = httpClient.post<LoginRequest, LoginResponse>(
+            route = "/login",
+            body = LoginRequest(
+                email = email,
+                password = password
+            )
+        )
+        if (result is Result.Success) {
+            sessionStorage.set(
+                AuthInfo(
+                    accessToken = result.data.accessToken,
+                    refreshToken = result.data.refreshToken,
+                    userId = result.data.userId,
+                )
+            )
+        }
+        return result.asEmptyDataResult()
+    }
 
     override suspend fun register(
         email: String,
