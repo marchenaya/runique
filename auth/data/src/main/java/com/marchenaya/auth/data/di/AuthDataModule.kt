20d35941037232e23467ed6231fb1288387cdca8
anna.marchenay <anna.marchanay@hubone.fr>
2026-06-15 17:37:56 +0200

    Refactor HTTP client injection and update AccessTokenResponse
    - Introduced HttpClientType enum to differentiate between authenticated and unauthenticated HTTP clients.
    - Updated CoreDataModule and AuthDataModule to use Koin's named qualifiers for HTTP client instances.
    - Changed the data type of expirationTimestamp from String to Long in AccessTokenResponse.
    - Updated AuthRepository to explicitly inject the unauthenticated HTTP client.

--- auth/data/src/main/java/com/marchenaya/auth/data/di/AuthDataModule.kt
+++ auth/data/src/main/java/com/marchenaya/auth/data/di/AuthDataModule.kt
@@ -6,15 +6,19 @@ import com.marchenaya.auth.data.networking.UnauthenticatedHttpClientFactory
 import com.marchenaya.auth.domain.AuthRepository
 import com.marchenaya.auth.domain.PatternValidator
 import com.marchenaya.auth.domain.UserDataValidator
+import com.marchenaya.core.data.networking.HttpClientType
 import org.koin.core.module.dsl.singleOf
+import org.koin.core.qualifier.named
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val authDataModule = module {
     singleOf(::UserDataValidator)
     singleOf(::EmailPatternValidator).bind<PatternValidator>()
-    singleOf(::AuthRepositoryImpl).bind<AuthRepository>()
-    single {
+    single<AuthRepository> {
+        AuthRepositoryImpl(get(named(HttpClientType.Unauthenticated)), get())
+    }
+    single(named(HttpClientType.Unauthenticated)) {
         UnauthenticatedHttpClientFactory().build()
     }
 }
